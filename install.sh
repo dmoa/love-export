34d46e74958c329fe14c4ffa1b0448e98d6725d9
dmoa <spammyboy@protonmail.com>
2020-02-19 15:52:20 +0000

    install script WIP
    attempt at mac installation...

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+INSTALL_PATH=$(pwd)
+
+git_clone() {
+    git clone https://github.com/dmoa/love-export
+    cd love-export
+    chmod +x main.sh
+    chmod +x install.sh
+}
+
+if [ "$(uname)" = "Darwin" ]; then
+    # Do something under Mac OS X platform
+    "echo" "detected MacOS"
+    git_clone
+    printf "%s\n" 'alias love-export="$INSTALL_PATH"/main.sh' >> ~/.zshrc
+    source ~/.zshrc
+elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
+    # Do something under GNU/Linux platform
+    "echo" "detected Linux"
+elif [ "$(expr substr $(uname -s) 1 9)" = "CYGWIN_NT" ]; then
+    # Do something under Windows NT platform
+    "echo" "detected Windows (Cygwin)"
+else
+    "echo" "ERROR: could not detect platform"
+    exit 0
+fi
