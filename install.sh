c2c79a6d87b72890a7d2ec0a31f49b4546b87d55
dmoa <spammyboy@protonmail.com>
2020-02-19 21:47:50 +0000

    added cygwin and linux installation feature

--- install.sh
+++ install.sh
@@ -18,10 +18,19 @@ if [ "$(uname)" = "Darwin" ]; then
 elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
     # Do something under GNU/Linux platform
     "echo" "detected Linux"
+    git_clone
+    printf "%s\n" 'alias love-export='$INSTALL_PATH'/love-export/main.sh' >> ~/.bashrc
+    bash
 elif [ "$(expr substr $(uname -s) 1 9)" = "CYGWIN_NT" ]; then
     # Do something under Windows NT platform
     "echo" "detected Windows (Cygwin)"
+    git_clone
+    printf "%s\n" 'alias love-export='$INSTALL_PATH'/love-export/main.sh' >> ~/.bashrc
+    bash
 else
     "echo" "ERROR: could not detect platform"
     exit 0
 fi
+
+"echo" "installation complete"
+exit 0
